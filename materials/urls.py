318967b76ec712307a825266f623244071e78085
Сергей <sergey.vasilev.98@mail.ru>
2024-11-24 18:15:47 +0300

    subscription urls

--- materials/urls.py
+++ materials/urls.py
@@ -4,7 +4,8 @@ from rest_framework.routers import SimpleRouter
 from materials.apps import MaterialsConfig
 from materials.views import (CoursesViewSet, LessonsCreateApiView,
                              LessonsDestroyAPIView, LessonsListAPIView,
-                             LessonsRetrieveAPIView, LessonsUpdateAPIView)
+                             LessonsRetrieveAPIView, LessonsUpdateAPIView, SubscriptionCreateAPIView,
+                             SubscriptionListAPIView)
 
 app_name = MaterialsConfig.name
 
@@ -14,22 +15,12 @@ router.register("", CoursesViewSet)
 
 urlpatterns = [
     path("materials/", LessonsListAPIView.as_view(), name="materials_list"),
-    path(
-        "materials/<int:pk>/",
-        LessonsRetrieveAPIView.as_view(),
-        name="materials_retrieve",
-    ),
-    path(
-        "materials/<int:pk>/update/",
-        LessonsUpdateAPIView.as_view(),
-        name="materials_update",
-    ),
+    path("materials/<int:pk>/", LessonsRetrieveAPIView.as_view(), name="materials_retrieve",),
+    path("materials/<int:pk>/update/", LessonsUpdateAPIView.as_view(), name="materials_update",),
     path("materials/create/", LessonsCreateApiView.as_view(), name="materials_create"),
-    path(
-        "materials/<int:pk>/destroy/",
-        LessonsDestroyAPIView.as_view(),
-        name="materials_destroy",
-    ),
+    path("materials/<int:pk>/destroy/", LessonsDestroyAPIView.as_view(), name="materials_destroy",),
+    path("subscription/create/", SubscriptionCreateAPIView.as_view(), name="subscription_create"),
+    path("subscription/", SubscriptionListAPIView.as_view(), name="subscriptions"),
 ]
 
 urlpatterns += router.urls
