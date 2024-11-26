3ac040d10ceca82dd247a6128a5779299c779244
Сергей <sergey.vasilev.98@mail.ru>
2024-11-26 21:50:13 +0300

    fix problem, documentations works

--- config/urls.py
+++ config/urls.py
@@ -37,7 +37,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("materials.urls", namespace="materials")),
+    path("materials/", include("materials.urls", namespace="materials")),
     path("users/", include("users.urls", namespace="users")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
