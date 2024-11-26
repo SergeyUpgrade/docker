6086bc1745f9ce766735dc83fed5e38a668d109a
Сергей <sergey.vasilev.98@mail.ru>
2024-11-26 21:30:50 +0300

    problem

--- config/settings.py
+++ config/settings.py
@@ -44,8 +44,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "drf_yasg",
     "users",
     "materials",
+
 ]
 
 MIDDLEWARE = [
